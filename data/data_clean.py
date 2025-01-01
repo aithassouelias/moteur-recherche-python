b9f372cccfc7eb306dd50bcda47d7728376c5ec1
aithassouelias <aithassouelias@gmail.com>
2025-01-01 18:41:55 +0100

    Ajout fonction search

--- data/data_clean.py
+++ data/data_clean.py
@@ -52,3 +52,37 @@ def clean_json(input_file, output_file):
 input_file = './data/data.json'  # Nom du fichier original
 output_file = './data/data_cleaned.json'  # Nom du fichier nettoyé
 clean_json(input_file, output_file)
+
+import numpy as np
+
+def search(mat_TFIDF, vocab):
+    # Obtenir la requête utilisateur
+    query = input("Entrez votre requête: ")
+    
+    # Diviser les mots de la requête
+    query_words = query.lower().split()
+    
+    # Initialiser le vecteur de requête
+    query_vec = np.zeros(len(vocab))
+    
+    # Remplir le vecteur de requête
+    for word in query_words:
+        if word in vocab:
+            query_vec[vocab[word]['unique_id']] = 1
+    
+    # Calculer la similarité cosinus entre la requête et les documents
+    query_norm = np.linalg.norm(query_vec)
+    if query_norm == 0:
+        print("Aucun mot de la requête n'est dans le vocabulaire.")
+        return -1
+    
+    doc_norms = np.linalg.norm(mat_TFIDF.todense(), axis=1)
+    cos_sim = np.zeros(len(doc_norms))
+    
+    for i, doc_norm in enumerate(doc_norms):
+        if doc_norm != 0:
+            cos_sim[i] = np.array(mat_TFIDF.todense()[i, :]).flatten().dot(query_vec) / (doc_norm * query_norm)
+    
+    # Sortir le document le plus similaire
+    most_similar_doc = np.argmax(cos_sim)
+    return cos_sim, most_similar_doc
